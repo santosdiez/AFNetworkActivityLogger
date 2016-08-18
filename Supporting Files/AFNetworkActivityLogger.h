2f8686e8f4accca4919075eef42254c87034f8dc
Borja Santos-Diez <borja.santos@mobgen.com>
2016-08-18 10:29:56 +0200

    Change imports

--- Supporting Files/AFNetworkActivityLogger.h
+++ Supporting Files/AFNetworkActivityLogger.h
@@ -31,6 +31,5 @@ FOUNDATION_EXPORT const unsigned char AFNetworkActivityLoggerVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <AFNetworkActivityLogger/PublicHeader.h>
 
 #import <AFNetworkActivityLogger/AFNetworkActivityLogger.h>
-#import <AFNetworkActivityLogger/AFNetworkActivityLoggerProtocol.h>
 #import <AFNetworkActivityLogger/AFNetworkActivityConsoleLogger.h>
-
+#import <AFNetworkActivityLogger/AFNetworkActivityLoggerProtocol.h>
